45ff62b61cebdf09fa26538eb862461db5e733f6
Misty <xiao02600@gmail.com>
2025-12-31 12:00:07 +0800

    style: 注释掉不必要的依赖项和代码

--- src/nonebot_plugin_template/__init__.py
+++ src/nonebot_plugin_template/__init__.py
@@ -1,10 +1,10 @@
-from nonebot import logger, require
+# from nonebot import logger, require
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 
-require("nonebot_plugin_uninfo")
-require("nonebot_plugin_alconna")
-require("nonebot_plugin_localstore")
-require("nonebot_plugin_apscheduler")
+# require("nonebot_plugin_uninfo")
+# require("nonebot_plugin_alconna")
+# require("nonebot_plugin_localstore")
+# require("nonebot_plugin_apscheduler")
 from .config import Config
 
 __plugin_meta__ = PluginMetadata(
@@ -20,26 +20,3 @@ __plugin_meta__ = PluginMetadata(
     # supported_adapters={"~onebot.v11"}, # 仅 onebot
     extra={"author": "owner <your@mail.com>"},
 )
-
-from arclet.alconna import Args, Option, Alconna, Arparma, Subcommand
-from nonebot_plugin_alconna import on_alconna
-from nonebot_plugin_alconna.uniseg import UniMessage
-
-pip = on_alconna(
-    Alconna(
-        "pip",
-        Subcommand(
-            "install",
-            Args["package", str],
-            Option("-r|--requirement", Args["file", str]),
-            Option("-i|--index-url", Args["url", str]),
-        ),
-    )
-)
-
-
-@pip.handle()
-async def _(result: Arparma):
-    package: str = result.other_args["package"]
-    logger.info(f"installing {package}")
-    await UniMessage.text(package).send()
